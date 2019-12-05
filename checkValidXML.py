d0227aa8db7422cfb0f122bfa8a734d6f94f132f
OpenLawsGR <plessas@ceid.upatras.gr>
2019-12-05 15:20:35 +0000

    Command Line Interface for performing validation of XML files based on Akoma Ntoso XSD

--- checkValidXML.py
+++ checkValidXML.py
@@ -1,38 +1,88 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import fnmatch
+import time
+import argparse
 from functions import CheckXMLvalidity
+from variables import *
 from lxml import etree
 
-# Folder where judgments
-BASE_XML_FOLDER =  os.path.join(os.getcwd(), 'XML')
+program_description = 'Validate XML files against Akoma Ntoso XSD schema '
 
-# Declare folder name
-FOLDER_PATH = r'ste/2013'
+parser = argparse.ArgumentParser(
+    description = program_description
+    )
 
-# Define folder path that parsing will start from 
-SRC = os.path.join(BASE_XML_FOLDER, FOLDER_PATH)
+legal_authority_help = 'choose a specific legal authority '
+legal_authority_help += '(accepted values: %(choices)s) to perform validation'
+parser.add_argument(
+    'legal_authority',
+    metavar = 'legal_authority',
+    choices = [AREIOS_PAGOS, STE, NSK],
+    help = legal_authority_help
+    )
+
+year_help = 'choose a specific year to perform validation'
+parser.add_argument(
+    '-year',
+    help = year_help
+    )
+
+# create a namespace object
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    print "Starting validation in : " + SRC
+    print args
+
+    source_path = os.path.join(
+        os.getcwd(),
+        os.path.join(
+            XML,
+            args.legal_authority
+            )
+        )
+
+    # legal opinions are not stored in folders by year of publication
+    if args.year is not None and args.legal_authority not in (NSK):
+        source_path = os.path.join(
+            source_path,
+            args.year
+            )
+        
+    #print source_path
+    print "Starting validation in : " + source_path
+    time.sleep(2)
+
+    total_files_cnt = 0
+    total_xml_files = 0
+    total_xml_valid = 0
+    total_xml_nonValid = 0
+    
     validXML = 0
     nonValidXML = 0
     XmlFiles = 0
     filesCnt = 0
-    for root, dirs, files in os.walk(SRC):  
+    for root, dirs, files in os.walk(source_path):  
         for name in files:
-            filesCnt += 1
-            if fnmatch.fnmatch(name, '*.xml'):
-                #print  name
-                XmlFiles += 1
-                if CheckXMLvalidity('akomantoso30.xsd', os.path.join(root, name)) == 1:
+            print name
+            total_files_cnt += 1
+            if fnmatch.fnmatch(name, '*' + XML_EXT):
+                total_xml_files += 1
+                if CheckXMLvalidity(
+                    'akomantoso30.xsd',
+                    os.path.join(
+                        root,
+                        name
+                        )
+                    ) == 1:
                     #print name + ": Valid"
-                    validXML += 1
+                    total_xml_valid += 1
                 else:
                     #print name + ": Not Valid"
-                    nonValidXML +=1
+                    total_xml_nonValid += 1
     
-    print "Total Files :" + str(filesCnt)
-    print "Total XML Files :" + str(XmlFiles)
-    print "Valid XML :" + str(validXML)
-    print "Non Valid XML : " + str(nonValidXML)
+    print "Total Files :" + str(total_files_cnt)
+    print "Total XML Files :" + str(total_xml_files)
+    print "Valid XML :" + str(total_xml_valid)
+    print "Non Valid XML : " + str(total_xml_nonValid)
